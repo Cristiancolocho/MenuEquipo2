330b0fd69b35f73b47ee3ea3994ac74bb055f7c4
CHATO LOKO <colochocristian72@gmail.com>
2024-06-27 21:48:16 -0600

    Agregar archivos de proyecto.

---
+++ MenuEquipo/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MenuEquipo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MenuPrincipal();
+        }
+    }
+}
---
+++ MenuEquipo/MenuPrincipal.xaml.cs
@@ -0,0 +1,35 @@
+namespace MenuEquipo;
+
+public partial class MenuPrincipal : ContentPage
+{
+	public MenuPrincipal()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        
+
+    }
+
+    private void Button_Clicked_1(object sender, EventArgs e)
+    {
+
+    }
+
+    private void Button_Clicked_2(object sender, EventArgs e)
+    {
+
+    }
+
+    private void Button_Clicked_3(object sender, EventArgs e)
+    {
+
+    }
+
+    private void Button_Clicked_4(object sender, EventArgs e)
+    {
+
+    }
+}
